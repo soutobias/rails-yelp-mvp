5682a0350a2bcd63b2ec63225f071a6df1d927b7
Tobias Ferreira <soutobias@yahoo.com.br>
2020-10-13 22:15:32 -0300

    yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Restaurant.destroy_all
+Review.destroy_all
+
+
+puts 'Creating cool Restaurants!'
+10.times {
+  Restaurant.create(
+  name: Faker::Restaurant.name,
+  address: Faker::Address.street_address,
+  phone_number: Faker::PhoneNumber.phone_number,
+  category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  )
+  5.times {
+    Review.create(
+      rating: [0, 1, 2, 3, 4, 5].sample,
+      content: Faker::TvShows::RuPaul.quote,
+      restaurant_id: Restaurant.maximum('id')
+      )
+  }
+}
